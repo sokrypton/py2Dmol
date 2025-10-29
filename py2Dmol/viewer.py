bf58ade5210124986498a6702988e196448e459e
Sergey O <kingsergey@gmail.com>
2025-10-29 09:36:26 -0400

    fixing to work in jupyter

--- py2Dmol/viewer.py
+++ py2Dmol/viewer.py
@@ -9,6 +9,7 @@ from IPython.display import display, HTML, Javascript
 import importlib.resources
 from . import resources as py2dmol_resources
 import gemmi
+import uuid
 
 def kabsch(a, b, return_v=False):
   """Computes the optimal rotation matrix for aligning a to b."""
@@ -42,7 +43,8 @@ class view:
         self._plddts = None
         self._chains = None
         self._atom_types = None
-        self._trajectory_counter = 0 # NEW: Counter for trials
+        self._trajectory_counter = 0
+        self._viewer_id = str(uuid.uuid4())  # Unique ID for this viewer instance
 
     def _serialize_data(self):
         """Serializes the current coordinate state to JSON."""
@@ -64,7 +66,7 @@ class view:
         self._coords = align_a_to_b(coords, self._coords)
 
       # Set defaults if not provided
-      if self._plddts is None: self._plddts = np.full(self._coords.shape[0], 50.0) # Default to 50
+      if self._plddts is None: self._plddts = np.full(self._coords.shape[0], 50.0)
       if self._chains is None: self._chains = ["A"] * self._coords.shape[0]
       if self._atom_types is None: self._atom_types = ["P"] * self._coords.shape[0]
 
@@ -85,8 +87,7 @@ class view:
           self._atom_types = ["P"] * self._coords.shape[0]
 
     def clear(self):
-        """MODIFIED: Clears the Python state and tells the JS viewer to start a new trajectory."""
-        # Generate a name for the new trajectory
+        """Clears the Python state and tells the JS viewer to start a new trajectory."""
         trajectory_name = f"{self._trajectory_counter}"
         self._trajectory_counter += 1
 
@@ -96,14 +97,21 @@ class view:
         self._chains = None
         self._atom_types = None
 
-        # NEW: Tell JS to create and switch to this new trajectory
+        # Tell JS to create and switch to this new trajectory
         if self._initial_data_loaded:
-            js_code = f"window.handlePythonNewTrajectory('{trajectory_name}');"
+            js_code = f"""
+            (function() {{
+                var iframe = document.querySelector('iframe[data-viewer-id="{self._viewer_id}"]');
+                if (iframe && iframe.contentWindow && iframe.contentWindow.handlePythonNewTrajectory) {{
+                    iframe.contentWindow.handlePythonNewTrajectory('{trajectory_name}');
+                }}
+            }})();
+            """
             if IS_COLAB:
                 try:
                     output.eval_js(js_code, ignore_result=True)
                 except Exception as e:
-                    pass
+                    print(f"Error clearing in Colab: {e}")
             else:
                 display(Javascript(js_code))
 
@@ -120,14 +128,14 @@ class view:
 
         viewer_config = {
             "size": self.size,
-            "color": self.color
+            "color": self.color,
+            "viewer_id": self._viewer_id
         }
         config_script = f"""
         <script id="viewer-config">
           window.viewerConfig = {json.dumps(viewer_config)};
         </script>
         """
-        # The data script now provides the *first frame* for the "Initial" trajectory
         data_script = f"""
         <script id="protein-data">
           window.proteinData = {self._serialize_data()};
@@ -136,22 +144,62 @@ class view:
         self._initial_data_loaded = True
 
         injection_scripts = config_script + "\n" + data_script
-        final_html = html_template.replace("<!-- DATA_INJECTION_POINT -->", injection_scripts)
-        display(HTML(final_html))
+        
+        # Wrap the HTML in an iframe with a unique ID for Jupyter compatibility
+        if not IS_COLAB:
+            # For Jupyter: wrap in iframe with srcdoc
+            final_html = html_template.replace("<!-- DATA_INJECTION_POINT -->", injection_scripts)
+            # Escape for srcdoc attribute
+            final_html_escaped = final_html.replace('"', '&quot;').replace("'", '&#39;')
+            iframe_html = f"""
+            <iframe 
+                data-viewer-id="{self._viewer_id}"
+                srcdoc="{final_html_escaped}"
+                style="width: {self.size[0] + 20}px; height: {self.size[1] + 80}px; border: none;"
+                sandbox="allow-scripts allow-same-origin"
+            ></iframe>
+            """
+            display(HTML(iframe_html))
+        else:
+            # For Colab: use direct HTML
+            final_html = html_template.replace("<!-- DATA_INJECTION_POINT -->", injection_scripts)
+            display(HTML(final_html))
 
     def add(self, coords, plddts=None, chains=None, atom_types=None):
-      """Sends a new frame of data to the JavaScript viewer."""
-      if self._initial_data_loaded:
-        self._update(coords, plddts, chains, atom_types)
-        json_data = self._serialize_data()
-        js_code = f"window.handlePythonUpdate(`{json_data}`);"
-        if IS_COLAB:
-            output.eval_js(js_code, ignore_result=True)
+        """Sends a new frame of data to the JavaScript viewer."""
+        if self._initial_data_loaded:
+            self._update(coords, plddts, chains, atom_types)
+            json_data = self._serialize_data()
+            
+            # Escape the JSON data properly for JavaScript
+            json_data_escaped = json_data.replace('\\', '\\\\').replace('`', '\\`').replace('$', '\\$')
+            
+            if IS_COLAB:
+                js_code = f"window.handlePythonUpdate(`{json_data_escaped}`);"
+                try:
+                    output.eval_js(js_code, ignore_result=True)
+                except Exception as e:
+                    print(f"Error in add (Colab): {e}")
+            else:
+                # For Jupyter: target the specific iframe
+                js_code = f"""
+                (function() {{
+                    var iframe = document.querySelector('iframe[data-viewer-id="{self._viewer_id}"]');
+                    if (iframe && iframe.contentWindow) {{
+                        if (iframe.contentWindow.handlePythonUpdate) {{
+                            iframe.contentWindow.handlePythonUpdate(`{json_data_escaped}`);
+                        }} else {{
+                            console.error('handlePythonUpdate not found in iframe. Viewer may not be fully loaded.');
+                        }}
+                    }} else {{
+                        console.error('Viewer iframe not found with ID: {self._viewer_id}');
+                    }}
+                }})();
+                """
+                display(Javascript(js_code))
         else:
-            display(Javascript(js_code))
-      else:
-        # If display() was never called, call it now
-        self.display(coords, plddts, chains, atom_types)
+            # If display() was never called, call it now
+            self.display(coords, plddts, chains, atom_types)
 
     update_data = add
 
@@ -211,8 +259,6 @@ class view:
                                 atom_chains.append(chain.name)
                                 
                                 # Distinguish RNA from DNA
-                                # RNA bases: A, C, G, U (and modified versions starting with R)
-                                # DNA bases: DA, DC, DG, DT (and A, C, G, T in some files)
                                 rna_bases = ['A', 'C', 'G', 'U', 'RA', 'RC', 'RG', 'RU']
                                 dna_bases = ['DA', 'DC', 'DG', 'DT', 'T']
                                 
@@ -221,7 +267,7 @@ class view:
                                 elif residue.name in dna_bases or residue.name.startswith('D'):
                                     atom_types.append('D')
                                 else:
-                                    # Default to RNA if uncertain (common in crystallography)
+                                    # Default to RNA if uncertain
                                     atom_types.append('R')
                                     
                         else:
