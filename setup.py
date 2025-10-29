b43548d1c0d171924f9568a271d88f789ae16254
Sergey O <kingsergey@gmail.com>
2025-10-29 12:28:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py2Dmol',
-    version='1.0.2',
+    version='1.1.0',
     author='sokrypton',
     author_email='so3@mit.edu',
     description='A Python library for visualizing protein structures in 2D.',
