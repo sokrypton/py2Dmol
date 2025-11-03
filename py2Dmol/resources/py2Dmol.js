c3450a32b95abdd988640aad578e3506c007be0c
Sergey O <kingsergey@gmail.com>
2025-11-02 21:57:47 -0500

    moving plddt selection to py2Dmol.js

--- py2Dmol/resources/py2Dmol.js
+++ py2Dmol/resources/py2Dmol.js
@@ -841,10 +841,19 @@ function initializePy2DmolViewer(containerElement) {
             
             // "auto" logic:
             // Always calculate what 'auto' *should* be
-            if (this.chains && this.chains.length > 0) {
+            const hasPAE = (paeData && paeData.length > 0);
+            
+            // --- NEW AUTO COLOR LOGIC ---
+            if (hasPAE) {
+                // If PAE data is present, 'auto' resolves to 'plddt'
+                this.resolvedAutoColor = 'plddt';
+            } else if (this.chains && this.chains.length > 0) {
+                // Otherwise, check chain count
                 const uniqueChains = new Set(this.chains.filter(c => c && c.trim()));
+                // If multi-chain, 'auto' resolves to 'chain', else 'rainbow'
                 this.resolvedAutoColor = (uniqueChains.size > 1) ? 'chain' : 'rainbow';
             } else {
+                // Fallback for empty chain data
                 this.resolvedAutoColor = 'rainbow';
             }
             
